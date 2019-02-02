f8676be9bb13b35b313a5e4d664219b670cb9023
川内翔一朗 <>
2019-02-03 04:15:49 +0900

    first commit

---
+++ FoodTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTutorial
+//
+//  Created by 川内翔一朗 on 2019/02/02.
+//  Copyright © 2019 川内翔一朗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
