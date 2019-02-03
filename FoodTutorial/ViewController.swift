85b7cb169e6f3b0f95b04cae1fe9c82a997ecaa2
川内翔一朗 <>
2019-02-03 11:59:16 +0900

    Connect the UI to Code

--- FoodTutorial/ViewController.swift
+++ FoodTutorial/ViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
     }
 
+    @IBAction func setDeafaulTextLabel(_ sender: Any) {
+        mealNameLabel.text = "Default Text"
+    }
+    //UITextFieldDelegate
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
 }
 
